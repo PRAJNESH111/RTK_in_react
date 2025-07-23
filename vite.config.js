9b246ab527380d44c92235c366f665bab4bbb9d8
PRAJNESH111 <prajneshpajju094@gmail.com>
2025-07-23 10:09:10 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/RTK_inRedux/",
+  base: "/RTK_in_react/", // ✅ MUST MATCH YOUR REPO NAME!
   plugins: [react()],
 });
